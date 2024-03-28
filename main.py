cd1ce7b1cfcdeef42e5f4f23c86ff564f9c93705
vigneshwarvj <iamvignesh1274@gmail.com>
2024-03-28 16:18:12 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from rembg import remove
+from PIL import Image
+
+def removebg(img):
+    input = Image.open(img)
+    return remove(input)
+
+def main():
+    st.title("Background Remover")
+    uploaded_file = st.file_uploader("Choose an Image...",type=["jpg","jpeg","png"])
+
+    if uploaded_file is not None:
+        st.image(uploaded_file, caption="Uploaded image")
+        st.write("Processing...")
+        result=removebg(uploaded_file)
+        st.image(result,caption="Result")
+
+if __name__ == '__main__':
+    main()
